bf6b83a00bfc4d8e5363891398495c6903d7fc2a
Emmanuel Gonzalez <emm.gs17@gmail.com>
2024-09-18 21:36:08 -0600

    Change the general layout

--- src/app/_components/more-stories.tsx
+++ src/app/_components/more-stories.tsx
@@ -7,11 +7,11 @@ type Props = {
 
 export function MoreStories({ posts }: Props) {
   return (
-    <section>
-      <h2 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
-        More Stories
+    <section className="flex flex-col items-center justify-center">
+      <h2 className="mb-8 text-3xl md:text-5xl font-bold tracking-tighter leading-tight">
+        Here are the lessons ↓
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+      <div className="w-1/2 grid grid-cols-1 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
         {posts.map((post) => (
           <PostPreview
             key={post.slug}

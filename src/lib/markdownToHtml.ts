8c8a77531c949365492ca6b807093c99d1e358b8
Emmanuel Gonzalez <emm.gs17@gmail.com>
2024-09-20 20:20:42 -0600

    feat: add syntax highlighting to the code snippets

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,20 @@
 import { remark } from "remark";
 import html from "remark-html";
+import { unified } from "unified";
+import remarkRehype from "remark-rehype";
+import remarkParse from "remark-parse";
+import rehypeFormat from "rehype-format";
+import rehypeHighlight from "rehype-highlight";
+import rehypeStringify from "rehype-stringify";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+  const result = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeHighlight)
+    .use(rehypeFormat)
+    .use(rehypeStringify)
+    .process(markdown);
+
   return result.toString();
 }

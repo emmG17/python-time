7f0e2ff88adc8c685c2b9444da26fd89853be79d
Emmanuel Gonzalez <emm.gs17@gmail.com>
2024-09-20 20:21:59 -0600

    fix: remove the unused imports

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,5 +1,3 @@
-import { remark } from "remark";
-import html from "remark-html";
 import { unified } from "unified";
 import remarkRehype from "remark-rehype";
 import remarkParse from "remark-parse";
